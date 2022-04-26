eb2ecec83224bc9d4173680b33e36c04888d6f35
Stuart Thiel Admin <Stuart Thiel Admin@erebus.encs.concordia.ca>
2022-04-26 14:53:54 -0400

    Fixed #2
    We didn't go with the script I'd used initially. Greg suggested making
    it python and I'd wanted to clean up the interface to make it consistent
    to what I wanted the Tootsuite tools to look like, so I've made a much
    simpler and fairly streamlined approach. I'll want to confirm it against
    the work I did in January, but tentatively it looks solid.

--- TooT-P/TooT-Proteome.py
+++ TooT-P/TooT-Proteome.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from Bio import SeqIO
+import csv
 import sys
 import os
 from pathlib import Path
@@ -9,13 +11,16 @@ import subprocess
 import traceback
 import logging
 
+TooTTScript = "TooT_TTool.R";
+TooTSCScript = "TooT_SCTool.R";
+
 parser = argparse.ArgumentParser(description="A tool to classify transporter proteins. It will filter a fasta-formatted file through TooT-T before sending it through TooT-SC and collating the results.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-query", help="a fasta formatted file", required=True)
-parser.add_argument("-work", help="a working directory for intermediate files", default=os.path.join(os.getcwd(), "work"))
+parser.add_argument("-work", help="a working directory for intermediate files", default=os.path.join(os.getcwd()))
 parser.add_argument("-out", help="an output directory where the final results will be stored", default=os.getcwd())
 parser.add_argument("-db", help="the location of the required TCDB and Swissprot databases", default=os.path.join(os.getcwd(), "db"))
-parser.add_argument("-TooTT", help="the location of the TooT-T script that will be run", default=shutil.which("TooT-TTool.R"))
-parser.add_argument("-TooTSC", help="the location of the TooT-SC script that will be run", default=shutil.which("TooT-SCTool.R"))
+parser.add_argument("-TooTT", help="the location of the TooT-T script that will be run", default=shutil.which(TooTTScript))
+parser.add_argument("-TooTSC", help="the location of the TooT-SC script that will be run", default=shutil.which(TooTSCScript))
 
 args = parser.parse_args()
 
@@ -55,30 +60,53 @@ if not os.access(args.out, os.W_OK):
     sys.exit("Output directory specifies a directory without permission to write: " + os.path.abspath(args.out))
 
 ##Validate that the work directory exists
-if not os.path.exists(args.work):
+TooTPWork = os.path.join(args.work, "work", "TooT-P");
+if not os.path.exists(TooTPWork):
     try:
-        Path(args.work).mkdir(parents=True, exist_ok=True)
+        print("Creating: " + TooTPWork)
+        Path(TooTPWork).mkdir(parents=True, exist_ok=True)
     except Exception as e:
         logging.error(traceback.format_exc())
-        sys.exit("There was a problem with the specified output directory: " + args.out)
-if not os.path.isdir(args.work):
-    sys.exit("Work directory specifies a file instead of a directory: " + os.path.abspath(args.work))
-if not os.access(args.work, os.W_OK):
-    sys.exit("Work directory specifies a directory without permission to write: " + os.path.abspath(args.work))
-
+        sys.exit("There was a problem creating working directories: " + os.path.abspath(TooTPWork))
+if not os.path.isdir(TooTPWork):
+    sys.exit("TooT-P work directory specifies a file instead of a directory: " + os.path.abspath(TooTPWork))
+if not os.access(TooTPWork, os.W_OK):
+    sys.exit("TooT-P work directory specifies a directory without permission to write: " + os.path.abspath(TooTPWork))
 
 
 # Run TooT-T on target query, with output going to <work>/TooT-Proteome/<query-filename>/
 TooTTCommand = args.TooTT + " -query=" + args.query + " -db=" + args.db + " -out=" + args.out + " -work=" + args.work
 print("Executing: " + TooTTCommand) 
-TooTTOut = subprocess.run([args.TooTT, "-query=" + args.query, "-db=" + args.db, "-out=" + args.out, "-work=" + args.work])
+TooTTOut = subprocess.run([args.TooTT, "-query=" + args.query, "-db=" + args.db, "-out=" + TooTPWork, "-work=" + args.work])
 print(TooTTOut.returncode)
 ## If exit code is bad, exit
 if TooTTOut.returncode != 0:
     sys.exit("TooT-T script returned non-zero exit! Aborting TooT-Proteome.")
 
-# Filter initial query into intermediate fasta file in <work>/TooT-Proteome/<query-filename>/<query-filename>
+
+# Filter initial query into intermediate fasta file (TooTTout.fasta in work dir) in <work>/TooT-P/TooTTout.fasta
+MembraneFasta = os.path.join(TooTPWork, "TooTTout.fasta")
+# Get rid of any old version
+if os.path.exists(MembraneFasta):
+    os.remove(MembraneFasta)
+
+MembraneProts = []
+
+with open(os.path.join(TooTPWork, "TooTTout.csv")) as csvFile:
+     csvReader = csv.reader(csvFile, delimiter=',')
+     for row in csvReader:
+         if row[2] == "1":
+             MembraneProts.append(row[1])
+for record in SeqIO.parse(args.query, "fasta"):
+    if record.id in MembraneProts:
+        with open(MembraneFasta, "a") as output_handle:
+            SeqIO.write(record, output_handle, "fasta")
+
+if not os.path.exists(MembraneFasta):
+    print("No membrane proteins were detected in: " + args.query)
+    sys.exit(0)
+
 # Run TooT-SC on the filtered query with output going to <work>/TooT-Proteome/<query-filename>/
-TooTSCOut = subprocess.run([args.TooTSC, "-query=" + os.path.join(args.work, ""), "-db=" + args.db, "-out=" + args.out, "-work=" + args.work])
+TooTSCOut = subprocess.run([args.TooTSC, "-query=" + MembraneFasta, "-db=" + args.db, "-out=" + args.out, "-work=" + args.work])
 ## If exit code is bad, exit
 # Copy output to <out>
